ac0173e7eb3a1047c17c686deba0a7b3c5ab1f20
Code Sandbox <hello@codesandbox.io>
2021-08-25 17:57:43 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.35f657a65f66ae35d5ace0a7a8234fb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058008954bb631ea9aa09cb24d552132",
+    "url": "https://veronica1981.github.io/csb-e7uhq/index.html"
+  },
+  {
+    "revision": "2bb407af08acbab6cd9d",
+    "url": "https://veronica1981.github.io/csb-e7uhq/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "535f734d3d070ada9220",
+    "url": "https://veronica1981.github.io/csb-e7uhq/static/js/2.43012d9f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://veronica1981.github.io/csb-e7uhq/static/js/2.43012d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb407af08acbab6cd9d",
+    "url": "https://veronica1981.github.io/csb-e7uhq/static/js/main.b2a16a49.chunk.js"
+  },
+  {
+    "revision": "4ab3b6cc37b794c679c6",
+    "url": "https://veronica1981.github.io/csb-e7uhq/static/js/runtime-main.f915836e.js"
+  }
+]);
